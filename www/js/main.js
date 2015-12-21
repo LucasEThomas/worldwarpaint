d21b62e2981154cc554f53618efe644b4b00ec05
vaporizationator <nospam4mester@gmail.com>
2015-12-21 16:27:22 -0600

    removed all demo junk

--- www/js/main.js
+++ www/js/main.js
@@ -6,9 +6,6 @@ function preload() {
 
     game.stage.backgroundColor = '#007236';
 
-    game.load.image('mushroom', 'assets/sprites/mushroom2.png');
-    game.load.image('sonic', 'assets/sprites/sonic_havok_sanity.png');
-    game.load.image('phaser', 'assets/sprites/asdf.png');
     game.load.image('background', 'assets/background.jpg');
     game.load.image('blueButton1', 'assets/BlueButton1.png');
     game.load.image('blueButton2', 'assets/BlueButton2.png');
@@ -43,33 +40,11 @@ function create() {
     gameBoardLayer.drawSplotch(500,500);
     gameBoardLayer.drawSplotch(200,200);
     //gameBoardLayer.drawCircles(300,300,100);
-    gameBoardLayer.drawBlob(600,300,200);
-    //add a random sprite 200 times. This is just for now.
-//    for (var i = 0; i < 200; i++)
-//    {
-//        game.add.sprite(game.world.randomX, game.world.randomY, 'mushroom');
-//    }
-
-//    game.add.text(0, 0, "this text scrolls\nwith the background", { font: "32px Arial", fill: "#f26c4f", align: "center" });
-    
-//    logo1 = game.add.sprite(0, 0, 'phaser');
-//    logo1.fixedToCamera = true;
-//    logo1.cameraOffset.setTo(100, 100);
-//
-//    logo2 = game.add.sprite(0, 0, 'phaser');
-//    logo2.fixedToCamera = true;
-//    logo2.cameraOffset.setTo(500, 100);
-
-//    var t = game.add.text(0, 0, "this text is fixed to the camera", { font: "32px Arial", fill: "#ffffff", align: "center" });
-//    t.fixedToCamera = true;
-//    t.cameraOffset.setTo(200, 500);
-
-//    game.add.tween(logo2.cameraOffset).to( { y: 400 }, 2000, Phaser.Easing.Back.InOut, true, 0, 2000, true);
+    //gameBoardLayer.drawBlob(600,300,200);
 
     cursors = game.input.keyboard.createCursorKeys();
     
     towerButton.makeButtons();
-
 }
 
 var keyboardScrollSpeed = 16;
