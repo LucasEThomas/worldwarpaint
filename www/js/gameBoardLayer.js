dec8ca3f2bfcaca489277195d1af93081c13e019
vaporizationator <nospam4mester@gmail.com>
2015-12-22 17:16:34 -0600

    drawRandomSprinkles() & drawRay() added.

--- www/js/gameBoardLayer.js
+++ www/js/gameBoardLayer.js
@@ -14,18 +14,18 @@ gameBoardLayer.initialize = function (){
 
 gameBoardLayer.mouseUp = function(){
     //logic for placing towers goes here?
-    gameBoardLayer.drawBlob(game.input.worldX, game.input.worldY, 50);
+    //gameBoardLayer.drawRandomBlob(game.input.worldX, game.input.worldY, 50);
+    //gameBoardLayer.drawRandomSprinkles(game.input.worldX, game.input.worldY, 50, 20);
+    gameBoardLayer.drawRay(game.input.worldX, game.input.worldY, 200, Math.random()*2*Math.PI, Math.PI*0.05);
 }
 
 gameBoardLayer.gameBoardBmd; //The game board layer. This is the semi-transparent layer where the players' paint colors are drawn.
 
-gameBoardLayer.drawBlob = function (x, y, radius){
+gameBoardLayer.drawRandomBlob = function (x, y, radius){
     var tension = radius*0.1;
     var controlPoints = gameBoardLayer.generateBlobControlPoints(x,y,radius,32);
     
-    this.gameBoardBmd.ctx.beginPath();
-    this.gameBoardBmd.ctx.fillStyle = '#0f6';
-    this.gameBoardBmd.ctx.globalAlpha = 0.5;
+    gameBoardLayer.setupDraw();
     this.gameBoardBmd.ctx.moveTo(controlPoints[0].x, controlPoints[0].y);
     
     for (var i = 0; i <= controlPoints.length - 1; i++){
@@ -41,15 +41,8 @@ gameBoardLayer.drawBlob = function (x, y, radius){
         
         this.gameBoardBmd.ctx.bezierCurveTo(cp1.x,cp1.y,cp2.x,cp2.y,nextPoint.x,nextPoint.y);
     }
-    this.gameBoardBmd.ctx.globalAlpha = 1;
-    this.gameBoardBmd.ctx.globalCompositeOperation = "destination-out";
-    this.gameBoardBmd.ctx.fill();
-    
-    this.gameBoardBmd.ctx.globalAlpha = 0.5;
-    this.gameBoardBmd.ctx.globalCompositeOperation = "source-over";
-    this.gameBoardBmd.ctx.fill();
+    gameBoardLayer.doDraw();
 }
-
 gameBoardLayer.generateBlobControlPoints = function(x, y, radius,count){
     var toReturn = [];
     for(var i = 0; i < count; i++){
@@ -59,3 +52,50 @@ gameBoardLayer.generateBlobControlPoints = function(x, y, radius,count){
     }
     return toReturn;
 }
+
+gameBoardLayer.drawRandomSprinkles = function (x,y,radius,count){
+    gameBoardLayer.setupDraw();
+    for(var i = 0; i < count; i++){
+        var currentRadius = radius * Math.random();
+        var currentDirection = 2 * Math.PI * Math.random();
+        var currentX = x + (currentRadius * Math.cos(currentDirection));
+        var currentY = y + (currentRadius * Math.sin(currentDirection));
+        this.gameBoardBmd.ctx.moveTo(currentX,currentY);
+        this.gameBoardBmd.ctx.ellipse(currentX, currentY,3,3,0,0,360,false);
+    }
+    gameBoardLayer.doDraw();
+}
+
+gameBoardLayer.drawRay = function (x,y,radius,angle,thicknessAngle){
+    gameBoardLayer.setupDraw();
+    var startAngle = angle - (thicknessAngle/2);
+    var endAngle = startAngle + thicknessAngle;
+    
+    var arcStartPoint = new Victor(x + (radius * Math.cos(startAngle)), y + (radius * Math.sin(startAngle)));
+    //var arcEndPoint = new Victor(x + (radius * Math.cos(direction+(Math.PI*0.1))), y + (radius * Math.sin(direction+(Math.PI*0.1))));
+    //var arcTangentPoint
+    
+    this.gameBoardBmd.ctx.moveTo(x,y);
+    this.gameBoardBmd.ctx.lineTo(arcStartPoint.x, arcStartPoint.y);
+    this.gameBoardBmd.ctx.arc(x,y,radius,startAngle,endAngle,false);
+    this.gameBoardBmd.ctx.lineTo(x,y);
+    gameBoardLayer.doDraw();
+}
+
+gameBoardLayer.setupDraw = function(){
+    this.gameBoardBmd.ctx.beginPath();
+    this.gameBoardBmd.ctx.fillStyle = '#0f6';
+    this.gameBoardBmd.ctx.globalAlpha = 0.5;
+}
+
+gameBoardLayer.doDraw = function(){
+    this.gameBoardBmd.ctx.globalAlpha = 1;
+    this.gameBoardBmd.ctx.globalCompositeOperation = "destination-out";
+    this.gameBoardBmd.ctx.fill();
+    
+    this.gameBoardBmd.ctx.globalAlpha = 0.5;
+    this.gameBoardBmd.ctx.globalCompositeOperation = "source-over";
+    this.gameBoardBmd.ctx.fill();
+}
+        
+        
