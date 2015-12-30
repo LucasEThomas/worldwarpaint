448ac46343fb7d33c187db634573b17c580d49d6
viznev <viznev@users.noreply.github.com>
2015-12-29 20:03:58 -0600

    Added tower class and tower event to message receive function

--- websocket_server/ws_server.js
+++ websocket_server/ws_server.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var WebSocketServer = require('ws').Server
   , wss = new WebSocketServer({ port: 8081 });
 
@@ -18,8 +20,26 @@ wss.on('connection', function connection(ws) {
         console.log('stopping client interval');
         clearInterval(id);
     });
-    ws.on('new tower', function incoming(message) {
-        console.log('received: %s', message);
+    
+    var towers = [];
+    
+    ws.on('message', function(data, flags) {
+        data = JSON.parse(data);
+        console.log('event rxd');
+        if (data.event === 'new tower') {
+            console.log('event: create tower');
+            var tower = new Tower(data.x, data.y, data.type);
+            towers.push(tower);
+            console.log(towers);
+        }
     });
 
 });
+
+class Tower {
+    constructor(x, y, type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    };
+};
