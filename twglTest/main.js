f69734718901a7bf2cb12f4cadb0a257f64f0696
vaporizationator <nospam4mester@gmail.com>
2016-01-14 17:38:39 -0600

    progress in webgl painting functionality

--- twglTest/main.js
+++ twglTest/main.js
@@ -4,22 +4,27 @@ var bufferCtx = canvasBuff.getContext("2d");
 var gl = getWebGLContext(canvasDest);
 
 var interval = {};
-function startGameLoop(){
-	initializeShaders();
-	interval = setInterval(() => {
-            circleX = Math.random()*127;
-            circleY = Math.random()*127;
-            bufferCtx.clearRect(0,0,canvasBuff.width,canvasBuff.height);
-            bufferCtx.beginPath();
-            bufferCtx.fillStyle = "red";
-            bufferCtx.moveTo(circleX, circleY)
-            bufferCtx.arc(circleY, circleX, 3, 0, Math.PI * 2, false);
-            bufferCtx.fill();
-            paintCanvas(0,0,canvasBuff.width,canvasBuff.height);
-        }, 500);
+
+function startGameLoop() {
+    initializeShaders();
+    interval = setInterval(() => {
+        var circleX = Math.round( Math.random() * 127);
+        if (circleX < 16) circleX = 16; else if (circleX >= 127 - 16) circleX = 127 - 16;
+            
+        
+        var circleY = Math.round( Math.random() * 127);
+        if (circleY < 16) circleY = 16; else if (circleY > 127 - 16) circleY = 127 - 16;
+        bufferCtx.clearRect(0, 0, canvasBuff.width, canvasBuff.height);
+        bufferCtx.beginPath();
+        bufferCtx.fillStyle = "red";
+        bufferCtx.moveTo(circleX, circleY)
+        bufferCtx.arc(circleX, circleY, 6, 0, Math.PI * 2, false);
+        bufferCtx.fill();
+        paintCanvas(circleX - 16, circleY - 16, 32, 32);
+    }, 500);
 }
 
-function initializeShaders(){
+function initializeShaders() {
 
     // Get A WebGL context
     if (!gl) {
@@ -32,26 +37,31 @@ function initializeShaders(){
     fragmentShader = createShaderFromScriptElement(gl, "2d-fragment-shader");
     program = createProgram(gl, [vertexShader, fragmentShader]);
     gl.useProgram(program);
+    gl.viewport(0, 0, canvasDest.width, canvasDest.height); //set the viewport to the whole display
 
     // look up where the vertex data needs to go.
     var positionLocation = gl.getAttribLocation(program, "a_position");
     var texCoordLocation = gl.getAttribLocation(program, "a_texCoord");
 
     // provide texture coordinates for the rectangle.
-    var texCoordBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-      0.0, 0.0,
-      1.0, 0.0,
-      0.0, 1.0,
-      0.0, 1.0,
-      1.0, 0.0,
-      1.0, 1.0]), gl.STATIC_DRAW);
-    gl.enableVertexAttribArray(texCoordLocation);
-    gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, false, 0, 0);
+//    var texCoordBuffer = gl.createBuffer();
+//    gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
+//    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+//      0.0, 0.0,
+//      1.0, 0.0,
+//      0.0, 1.0,
+//      0.0, 1.0,
+//      1.0, 0.0,
+//      1.0, 1.0]), gl.STATIC_DRAW);
+//    gl.enableVertexAttribArray(texCoordLocation);
+//    gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, false, 0, 0);
 
     // lookup uniforms
     resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+    buffResolutionLocation = gl.getUniformLocation(program, "u_buff_resolution");
+    buffPositionLocation = gl.getUniformLocation(program, "u_buff_position");
+    vxDrawFromBufferLocation = gl.getUniformLocation(program, "u_vxDrawFromBuffer");
+    fgDrawFromBufferLocation = gl.getUniformLocation(program, "u_fgDrawFromBuffer");
 
     // set the resolution
     gl.uniform2f(resolutionLocation, canvasDest.width, canvasDest.height);
@@ -88,20 +98,24 @@ function initializeShaders(){
     texBuff = setupTexture(canvasBuff, 0, program, "u_canvasBuff");
     texDest1 = setupTexture(canvasDest, 1, program, "u_canvasDest");
     texDest2 = setupTexture(canvasDest, 2, program, "u_canvasDest");
-    
+
     // Create a framebuffer and attach texDest1 to it
     fbo1 = gl.createFramebuffer();
     gl.bindFramebuffer(gl.FRAMEBUFFER, fbo1);
     gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texDest1, 0);
-    
+
     // Create a framebuffer and attach texDest2 to it
     fbo2 = gl.createFramebuffer();
     gl.bindFramebuffer(gl.FRAMEBUFFER, fbo2);
     gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texDest2, 0);
-    
+
     //gl.preserveDrawingBuffer = true;
 }
 var resolutionLocation;
+var buffResolutionLocation;
+var buffPositionLocation;
+var vxDrawFromBufferLocation;
+var fgDrawFromBufferLocation;
 var texBuff;
 var texDest1;
 var texDest2;
@@ -109,35 +123,44 @@ var fbo1;
 var fbo2;
 
 var toggle = true;
-function paintCanvas(x,y,width,height) {
+
+function paintCanvas(x, y, width, height) {
     //load in the buffer canvas data
-    gl.uniform1f(gl.getUniformLocation(program, "u_flipY"), 1);
     gl.activeTexture(gl.TEXTURE0);
-    gl.texSubImage2D(gl.TEXTURE_2D, 0, x, y, width, height, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array(bufferCtx.getImageData(x,y,width,height).data));
+    setRectangle(gl, x, y, width, height);
+    gl.texSubImage2D(gl.TEXTURE_2D, 0, x, y, width, height, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array(bufferCtx.getImageData(x, y, width, height).data));
+
+    //setup uniforms for combining buffer with destination data
+    gl.uniform1f(gl.getUniformLocation(program, "u_flipY"), 1); //flip the y axis
+    gl.uniform1f(vxDrawFromBufferLocation, 1); //combine buffer and destination data
+    gl.uniform1f(fgDrawFromBufferLocation, 1); //combine buffer and destination data
+
+    gl.uniform2f(resolutionLocation, canvasDest.width, canvasDest.height); //Tell the shader the resolution of the destination.
+    gl.uniform2f(buffPositionLocation, x, y); //Tell the shader the position of the buffer.
+    gl.uniform2f(buffResolutionLocation, width, height); //Tell the shader the resolution of the buffer.
+
+    gl.activeTexture(gl.TEXTURE0 + (toggle ? 2 : 1)); //set the active texture (why? what does this do?)
+    gl.bindTexture(gl.TEXTURE_2D, toggle ? texDest2 : texDest1); //bind the active texture
     
-    gl.activeTexture(gl.TEXTURE0 + (toggle?2:1));
-    gl.bindTexture(gl.TEXTURE_2D, toggle?texDest2:texDest1);
     //ping pong between framebuffers rendering each successive result as an aggregate composite of the previous ones
-    if(toggle){
-        gl.bindFramebuffer(gl.FRAMEBUFFER, fbo1);           //set the framebuffer we're rendering into (output)
-        gl.uniform2f(resolutionLocation, width, height);    //Tell the shader the resolution of the framebuffer.
-        gl.viewport(0, 0, width, height);                   //Tell webgl the viewport setting needed for framebuffer.
-        gl.drawArrays(gl.TRIANGLES, 0, 6);                  //do the draw
-    } else{
-        gl.bindFramebuffer(gl.FRAMEBUFFER, fbo2);           //set the framebuffer we're rendering into (output)
-        gl.uniform2f(resolutionLocation, width, height);    //Tell the shader the resolution of the framebuffer.
-        gl.viewport(0, 0, width, height);                   //Tell webgl the viewport setting needed for framebuffer.
-        gl.drawArrays(gl.TRIANGLES, 0, 6);                  //do the draw
+    if (toggle) {
+        gl.bindFramebuffer(gl.FRAMEBUFFER, fbo1); //set the framebuffer we're rendering into (output)
+        gl.drawArrays(gl.TRIANGLES, 0, 6); //do the draw
+    } else {
+        gl.bindFramebuffer(gl.FRAMEBUFFER, fbo2); //set the framebuffer we're rendering into (output)
+        gl.drawArrays(gl.TRIANGLES, 0, 6); //do the draw
     }
-    
+
     //render the result to the screen
-    gl.uniform1f(gl.getUniformLocation(program, "u_flipY"), -1);
-    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-    gl.drawArrays(gl.TRIANGLES, 0, 6); 
-    
-    //set the next texture (input)
-    gl.bindTexture(gl.TEXTURE_2D, toggle?texDest1:texDest2);
-    toggle = !toggle;
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null); //now, render to the screen not a framebuffer
+    gl.uniform1f(vxDrawFromBufferLocation, 0); //render only the destination texture to the screen
+    gl.uniform1f(fgDrawFromBufferLocation, 0); //render only the destination texture to the screen
+    setRectangle(gl, x, y, width, height);
+    gl.uniform1f(gl.getUniformLocation(program, "u_flipY"), -1); //don't flip the y axis for this operation
+    gl.bindTexture(gl.TEXTURE_2D, toggle ? texDest1 : texDest2); //bind the last destination texture as the input to this draw
+    gl.drawArrays(gl.TRIANGLES, 0, 6); //do the draw
+
+    toggle = !toggle; //toggle
 }
 
 function setRectangle(gl, x, y, width, height) {
