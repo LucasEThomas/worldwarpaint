c8c6ce192d1c09b89b8fa7edce371a145c1da2e0
vaporizationator <nospam4mester@gmail.com>
2016-01-19 16:40:38 -0600

    canvas pingponging and shader works!

--- twglTest/main.js
+++ twglTest/main.js
@@ -1,12 +1,13 @@
 var canvasBuff = document.getElementById("canvasBuffer");
 var canvasDest = document.getElementById("canvasDest");
 var bufferCtx = canvasBuff.getContext("2d");
-var gl = getWebGLContext(canvasDest);
+var gl = getWebGLContext(canvasDest, {preserveDrawingBuffer: true});
 
 var interval = {};
 
 function startGameLoop() {
     initializeShaders();
+    
     interval = setInterval(() => {
         var circleX = Math.round( Math.random() * 127);
         if (circleX < 16) circleX = 16; else if (circleX >= 127 - 16) circleX = 127 - 16;
@@ -41,7 +42,7 @@ function initializeShaders() {
 
     // look up where the vertex data needs to go.
     var positionLocation = gl.getAttribLocation(program, "a_position");
-    var texCoordLocation = gl.getAttribLocation(program, "a_texCoord");
+    //var texCoordLocation = gl.getAttribLocation(program, "a_texCoord");
 
     // provide texture coordinates for the rectangle.
 //    var texCoordBuffer = gl.createBuffer();
@@ -58,8 +59,8 @@ function initializeShaders() {
 
     // lookup uniforms
     resolutionLocation = gl.getUniformLocation(program, "u_resolution");
-    buffResolutionLocation = gl.getUniformLocation(program, "u_buff_resolution");
-    buffPositionLocation = gl.getUniformLocation(program, "u_buff_position");
+    //buffResolutionLocation = gl.getUniformLocation(program, "u_buff_resolution");
+    //buffPositionLocation = gl.getUniformLocation(program, "u_buff_position");
     vxDrawFromBufferLocation = gl.getUniformLocation(program, "u_vxDrawFromBuffer");
     fgDrawFromBufferLocation = gl.getUniformLocation(program, "u_fgDrawFromBuffer");
 
@@ -96,8 +97,9 @@ function initializeShaders() {
 
     //setup the textures
     texBuff = setupTexture(canvasBuff, 0, program, "u_canvasBuff");
-    texDest1 = setupTexture(canvasDest, 1, program, "u_canvasDest");
-    texDest2 = setupTexture(canvasDest, 2, program, "u_canvasDest");
+    texDest1 = setupTexture(canvasDest, 1, program, "u_canvasDest1");
+    texDest2 = setupTexture(canvasDest, 2, program, "u_canvasDest1");
+    u_canvasDestLocation = gl.getUniformLocation(program, "u_canvasDest");
 
     // Create a framebuffer and attach texDest1 to it
     fbo1 = gl.createFramebuffer();
@@ -112,8 +114,9 @@ function initializeShaders() {
     //gl.preserveDrawingBuffer = true;
 }
 var resolutionLocation;
-var buffResolutionLocation;
-var buffPositionLocation;
+//var buffResolutionLocation;
+//var buffPositionLocation;
+var u_canvasDestLocation;
 var vxDrawFromBufferLocation;
 var fgDrawFromBufferLocation;
 var texBuff;
@@ -127,20 +130,21 @@ var toggle = true;
 function paintCanvas(x, y, width, height) {
     //load in the buffer canvas data
     gl.activeTexture(gl.TEXTURE0);
-    setRectangle(gl, x, y, width, height);
+    gl.bindTexture(gl.TEXTURE_2D, texBuff);
     gl.texSubImage2D(gl.TEXTURE_2D, 0, x, y, width, height, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array(bufferCtx.getImageData(x, y, width, height).data));
+    
+    //tell the shader the size to update
+    setRectangle(gl, x, y, width, height);
 
     //setup uniforms for combining buffer with destination data
     gl.uniform1f(gl.getUniformLocation(program, "u_flipY"), 1); //flip the y axis
+    //gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true); //todo use this instead
     gl.uniform1f(vxDrawFromBufferLocation, 1); //combine buffer and destination data
     gl.uniform1f(fgDrawFromBufferLocation, 1); //combine buffer and destination data
 
-    gl.uniform2f(resolutionLocation, canvasDest.width, canvasDest.height); //Tell the shader the resolution of the destination.
-    gl.uniform2f(buffPositionLocation, x, y); //Tell the shader the position of the buffer.
-    gl.uniform2f(buffResolutionLocation, width, height); //Tell the shader the resolution of the buffer.
-
+    gl.uniform1i(u_canvasDestLocation, (toggle ? 2 : 1));
     gl.activeTexture(gl.TEXTURE0 + (toggle ? 2 : 1)); //set the active texture (why? what does this do?)
-    gl.bindTexture(gl.TEXTURE_2D, toggle ? texDest2 : texDest1); //bind the active texture
+    gl.bindTexture(gl.TEXTURE_2D, toggle ? texDest2 : texDest1);
     
     //ping pong between framebuffers rendering each successive result as an aggregate composite of the previous ones
     if (toggle) {
@@ -150,14 +154,24 @@ function paintCanvas(x, y, width, height) {
         gl.bindFramebuffer(gl.FRAMEBUFFER, fbo2); //set the framebuffer we're rendering into (output)
         gl.drawArrays(gl.TRIANGLES, 0, 6); //do the draw
     }
-
-    //render the result to the screen
-    gl.bindFramebuffer(gl.FRAMEBUFFER, null); //now, render to the screen not a framebuffer
+    
+    //copy to prev canvas
     gl.uniform1f(vxDrawFromBufferLocation, 0); //render only the destination texture to the screen
     gl.uniform1f(fgDrawFromBufferLocation, 0); //render only the destination texture to the screen
-    setRectangle(gl, x, y, width, height);
-    gl.uniform1f(gl.getUniformLocation(program, "u_flipY"), -1); //don't flip the y axis for this operation
+    gl.uniform1i(u_canvasDestLocation, (toggle ? 1 : 2));
+    gl.activeTexture(gl.TEXTURE0 + (toggle ? 1 : 2));
     gl.bindTexture(gl.TEXTURE_2D, toggle ? texDest1 : texDest2); //bind the last destination texture as the input to this draw
+    if (toggle) {
+        gl.bindFramebuffer(gl.FRAMEBUFFER, fbo2); //set the framebuffer we're rendering into (output)
+        gl.drawArrays(gl.TRIANGLES, 0, 6); //do the draw
+    } else {
+        gl.bindFramebuffer(gl.FRAMEBUFFER, fbo1); //set the framebuffer we're rendering into (output)
+        gl.drawArrays(gl.TRIANGLES, 0, 6); //do the draw
+    }
+    
+    //render the result to the screen
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null); //now, render to the screen not a framebuffer
+    gl.uniform1f(gl.getUniformLocation(program, "u_flipY"), -1); //don't flip the y axis for this operation
     gl.drawArrays(gl.TRIANGLES, 0, 6); //do the draw
 
     toggle = !toggle; //toggle
