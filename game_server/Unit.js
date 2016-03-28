6401ab86d6d6afa0af5d3ddea1020d5de9ed0220
vaporizationator <nospam4mester@gmail.com>
2016-03-28 14:21:09 -0500

    Unit.js refactoring

--- game_server/Unit.js
+++ game_server/Unit.js
@@ -1,23 +1,21 @@
 "use strict";
 var Victor = require('victor');
 
+var unitTypes = ['champion', 'archer', 'sprinklerTower'];
+
 class Unit {
     constructor(id, x, y, type, owner) {
         this.id = id;
-        this.x = x;
+        this.x = x; //units current position
         this.y = y;
+        this.destX = x; //if the unit is moving, it's current destination
+        this.destY = y;
         this.type = type;
         this.ownerID = owner;
-        if(type === 'hero'){
-            this.destX = x;
-            this.destY = y;
-        }
     };
-    setDestination(x,y){
-        if(this.type === 'hero'){
-            this.destX = x;
-            this.destY = y;
-        }
+    setDestination(x, y) {
+        this.destX = x;
+        this.destY = y;
     }
     generateEvents() {
         //this method will generate all sorts of events for each type of unit
@@ -29,62 +27,53 @@ class Unit {
             var randomDirection = Math.TWOPI * Math.random();
             var splatterX = Math.round(this.x + (unitRange * Math.cos(randomDirection)));
             var splatterY = Math.round(this.y + (unitRange * Math.sin(randomDirection)));
-            var splatterRadius = Math.round(Math.getRandomArbitrary(15,32));
-            //var controlPoints = this.generateBlobControlPoints(currentX, currentY, currentRadius, 32);
-            if(splatterX - splatterRadius > 0 && splatterX + splatterRadius < 2047 && splatterY - splatterRadius > 0 && splatterY + splatterRadius < 2047)
-            sprinkles.push({
-                x:splatterX,
-                y:splatterY,
-                radius:splatterRadius,
-                inputIndex: Math.round(Math.getRandomArbitrary(96,159))
-            });
+            var splatterRadius = Math.round(Math.getRandomArbitrary(15, 32));
+            if (splatterX - splatterRadius > 0 && splatterX + splatterRadius < 2047 && splatterY - splatterRadius > 0 && splatterY + splatterRadius < 2047)
+                sprinkles.push({
+                    x: splatterX,
+                    y: splatterY,
+                    radius: splatterRadius,
+                    inputIndex: Math.round(Math.getRandomArbitrary(96, 159))
+                });
         }
         toReturn.push({
             type: 'sprinklerUnit',
             data: sprinkles
         });
-        
 
-        
-        if(this.type === 'hero' && (this.destX !== this.x || this.destY !== this.y)){
-            var currentPoint = new Victor(this.x, this.y);
-            var destPoint = new Victor(this.destX, this.destY);
-            var direction = destPoint.clone().subtract(currentPoint).direction();
-            var distance = currentPoint.distance(destPoint);
-            if(distance <= 5){
-                this.x = this.destX;
-                this.y = this.destY;
-            }
-            else{
-                var deltaVector = new Victor(5,0).rotate(direction);
-                this.x += deltaVector.x;
-                this.y += deltaVector.y;
-            }
-            
-            toReturn.push({
-                type: 'moveUnit',
-                data: {
-                    x:this.x,
-                    y:this.y,
-                    id:this.id
-                }
-            });
+        if (this.destX !== this.x || this.destY !== this.y) {
+            toReturn.push(generateMoveEvent());
         }
-        
         return toReturn;
     }
-    generateBlobControlPoints(x, y, radius, count) {
-    var toReturn = [];
-    for (var i = 0; i < count; i++) {
-        var angle = Math.TWOPI * i / count;
-        var randomRadius = radius + radius * Math.getRandomArbitrary(-0.1, 0.1);
-        toReturn.push({
-            x:Math.round(x + (randomRadius * Math.cos(angle))), 
-            y:Math.round(y + (randomRadius * Math.sin(angle)))
-        })
+    
+    generateSprinkleEvents(){
+        
     }
-    return toReturn;
+
+    generateMoveEvent() {
+        var currentPoint = new Victor(this.x, this.y);
+        var destPoint = new Victor(this.destX, this.destY);
+        var direction = destPoint.clone().subtract(currentPoint).direction();
+        var distance = currentPoint.distance(destPoint);
+        if (distance <= 5) {
+            this.x = this.destX;
+            this.y = this.destY;
+        } else {
+            var deltaVector = new Victor(5, 0).rotate(direction);
+            this.x += deltaVector.x;
+            this.y += deltaVector.y;
+        }
+
+        return {
+            type: 'moveUnit',
+            data: {
+                x: this.x,
+                y: this.y,
+                id: this.id
+            }
+        };
+    }
 }
-};
 
 module.exports = Unit;
