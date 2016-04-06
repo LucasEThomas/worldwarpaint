5f993dd87e68fe6e12104fd821618d78f4078b58
vaporizationator <nospam4mester@gmail.com>
2016-04-06 15:02:15 -0500

    Map.js cleanup

--- app_server/public/js/Map.js
+++ app_server/public/js/Map.js
@@ -2,11 +2,7 @@
 
 class Map extends Phaser.RenderTexture{
     constructor() {
-        //todo, this may need to be make.
-        //new RenderTexture(game, key, width, height, key, scaleMode, resolution)
         super(game, 3548, 2048, 'terrainBackground');
-        
-//        return game.make.renderTexture(3548, 2048, 'terrainBackground');
     }
 
     setTerrainMap(terrainMap) {
@@ -75,33 +71,6 @@ class Map extends Phaser.RenderTexture{
             }
             gridX++;
         }
-    }
-    spawnRenderTextureTiles() {//DEPRECATED
-        game.terrainRenderTexture = game.add.renderTexture(3548, 2048, 'terrainBackground');
-        var noteTile = game.make.sprite(0, 0, 'notebookPaper');
-        noteTile.anchor.set(.5);
-        for (var nx = 0; nx < 4096; nx += 1132) {
-            for (var ny = 0; ny < 4096; ny += 654) {
-                game.terrainRenderTexture.renderXY(noteTile, nx, ny);
-                game.terrainRenderTexture.renderXY(noteTile, nx + 1132 / 2, ny + 654 / 2);
-            }
-        }
-        for (var xx = 0; xx < 2048; xx += 45) {
-            for (var yy = 0; yy < 2048; yy += 45) {
-                // Create a tile using the new game.add.isoSprite factory method at the specified position.
-                // The last parameter is the group you want to add it to (just like game.add.sprite)
-                var tile = (Math.random() < .9) ? game.make.sprite(0, 0, 'grass1') : game.make.sprite(0, 0, 'grass2');
-                tile.anchor.set(0.5);
-                var point = game.iso.projectXY({
-                    x: xx,
-                    y: yy,
-                    z: 0
-                });
-                //console.log(point);
-                game.terrainRenderTexture.renderXY(tile, point.x, point.y);
-            }
-        }
-        game.add.sprite(0, 0, game.terrainRenderTexture);
     }
 }
 
