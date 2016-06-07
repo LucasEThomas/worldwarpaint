198e7ed5c032b2a623c806cac592a2800a2c908e
vaporizationator <nospam4mester@gmail.com>
2016-06-07 17:15:31 -0500

    heal ray appearance

--- app_server/public/js/units/healerTower.js
+++ app_server/public/js/units/healerTower.js
@@ -3,36 +3,36 @@ class HealerTower extends Unit {
         super(x, y, id, ownerId, 'tower', 100, onKilled);
         this.type = 'healerTower';
 
-//        let sqrDistTo = (unit) => {
-//            let distX = this.sprite.isoX - unit.sprite.isoX;
-//            let distY = this.sprite.isoY - unit.sprite.isoY;
-//            let distSqr = Math.pow(distX, 2) + Math.pow(distY, 2);
-//            return distSqr;
-//        }
-//
-//        let testCanHeal = (unit) => {
-//            if (unit) {
-//                let maxRangeSqr = Math.pow(200, 2);
-//                let distSqr = sqrDistTo(unit);
-//
-//                return distSqr <= maxRangeSqr && unit.clrName === this.clrName && unit.id != this.id;
-//            } else {
-//                return false;
-//            }
-//        }
-//
-//        let targets = game.units.units.filter((nUnit) => testCanHeal(nUnit));
-//        targets.forEach((nTarget, n) => {
-//            HealerTower.healBeam({
-//                x: this.sprite.isoX,
-//                y: this.sprite.isoY,
-//                z: 35
-//            }, {
-//                x: nTarget.sprite.isoX,
-//                y: nTarget.sprite.isoY,
-//                z: 35
-//            });
-//        });
+        //        let sqrDistTo = (unit) => {
+        //            let distX = this.sprite.isoX - unit.sprite.isoX;
+        //            let distY = this.sprite.isoY - unit.sprite.isoY;
+        //            let distSqr = Math.pow(distX, 2) + Math.pow(distY, 2);
+        //            return distSqr;
+        //        }
+        //
+        //        let testCanHeal = (unit) => {
+        //            if (unit) {
+        //                let maxRangeSqr = Math.pow(200, 2);
+        //                let distSqr = sqrDistTo(unit);
+        //
+        //                return distSqr <= maxRangeSqr && unit.clrName === this.clrName && unit.id != this.id;
+        //            } else {
+        //                return false;
+        //            }
+        //        }
+        //
+        //        let targets = game.units.units.filter((nUnit) => testCanHeal(nUnit));
+        //        targets.forEach((nTarget, n) => {
+        //            HealerTower.healBeam({
+        //                x: this.sprite.isoX,
+        //                y: this.sprite.isoY,
+        //                z: 35
+        //            }, {
+        //                x: nTarget.sprite.isoX,
+        //                y: nTarget.sprite.isoY,
+        //                z: 35
+        //            });
+        //        });
 
     }
     processEvent(event) {
@@ -46,7 +46,7 @@ class HealerTower extends Unit {
                 //start points in a circle around the top of the tower
                 let newStart = new Victor(12, 12).rotate(endPoint.clone().subtract(startPoint).angle() - Math.TWOPI * 0.25).add(startPoint);
 
-                HealerTower.fireHealer({
+                HealerTower.fireHealRay({
                     x: newStart.x,
                     y: newStart.y,
                     z: 35
@@ -60,41 +60,8 @@ class HealerTower extends Unit {
             }
         }
     }
-    static healBeam(startPointIsoSpace, endPointIsoSpace) {
-        let startPoint = Victor.fromObject(game.iso.project(startPointIsoSpace));
-        let endPoint1 = Victor.fromObject(game.iso.project(endPointIsoSpace));
 
-        let distance = Math.round(startPoint.distance(endPoint1));
-        let angle = endPoint1.clone().subtract(startPoint).angleDeg();
-        
-
-        let sprite = game.add.sprite(startPoint.x, startPoint.y);
-
-
-        let sectionSize = distance / 20;
-        let points = [];
-
-        for (var i = 0; i < 20; i++) {
-            points.push(new Phaser.Point(i * sectionSize, 0));
-        }
-
-        let beamRope = game.add.rope(startPoint.x, startPoint.y, 'healer_beam', null, points);
-//        beamRope.counterasdf = 0;
-        beamRope.blendMode = PIXI.blendModes.ADD;
-        beamRope.alpha = 0.5;
-        beamRope.scale.setTo(1, 0.5);
-        beamRope.angle = angle;
-
-//        beamRope.updateAnimation = () => {
-//            beamRope.counterasdf += 0.03;
-//
-//            for (var i = 0; i < points.length; i++) {
-//                points[i].y = Math.sin(i * 0.5 + beamRope.counterasdf) * 8;
-//            }
-//        };
-    }
-    
-    static fireHealer(startPointIsoSpace, endPointIsoSpace, impactCallback) {
+    static fireHealRay(startPointIsoSpace, endPointIsoSpace, impactCallback) {
         let startPoint = Victor.fromObject(game.iso.project(startPointIsoSpace));
         let endPoint1 = Victor.fromObject(game.iso.project(endPointIsoSpace));
         endPointIsoSpace.z = 35;
@@ -120,13 +87,17 @@ class HealerTower extends Unit {
         endSprite.y += 20;
         endSprite.angle = 180;
 
-        sprite.angle = initialAngle;
+
+        sprite.baseAngle = initialAngle;
         let linearTween = game.add.tween(sprite).to({
-            angle: finalAngle,
+            baseAngle: finalAngle,
             alpha: 0,
         }, 1000, null, true).onComplete.add(() => {
             sprite.destroy();
         });
+        sprite.update = () => {
+            sprite.angle = 10 * Math.random() + sprite.baseAngle;
+        };
 
         impactCallback();
     }
