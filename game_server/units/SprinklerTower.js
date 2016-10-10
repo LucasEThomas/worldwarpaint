72689e63719e53d3644a2136215b4be105e3bbdf
vaporizationator <nospam4mester@gmail.com>
2016-10-10 16:06:29 -0500

    sprinkler tower shoots 5 at a time

--- game_server/units/SprinklerTower.js
+++ game_server/units/SprinklerTower.js
@@ -6,11 +6,11 @@ class SprinklerTower extends Unit {
         super(id, x, y, 'sprinklerTower', owner, gameState);
 
         this.sprinklerAngle = 0;
-        this.sprinklerRangeMin = 10;
-        this.sprinklerRangeMax = 150;
-        this.sprinklerROFInterval = 10;
-        this.sprinklerSplashRadius = 10;
-        this.sprinklerSpinRate = Math.TWOPI / 49; //spin per 20th of a second.
+        this.sprinklerRangeMin = 100;
+        this.sprinklerRangeMax = 400;
+        this.sprinklerROFInterval = 500;
+        this.sprinklerSplashRadius = 30;
+        this.sprinklerSpinRate = Math.TWOPI / 11; //spin per tick.
         this.sprinklerHeads = 1;
 
         this.lastSprinkleTimestamp = Date.now();
@@ -20,27 +20,37 @@ class SprinklerTower extends Unit {
         let events = [];
         this.updateProperties(time);
         if (time - this.lastSprinkleTimestamp >= this.sprinklerROFInterval) {
-            let splatterDistance = Math.getRandomArbitrary(this.sprinklerRangeMin, this.sprinklerRangeMax);
-            let splatterDirection = this.sprinklerAngle;
-            let splatterX = Math.round(this.x + (splatterDistance * Math.cos(splatterDirection)));
-            let splatterY = Math.round(this.y + (splatterDistance * Math.sin(splatterDirection)));
-            let splatterRadius = Math.round(Math.getRandomArbitrary(this.sprinklerSplashRadius - 2, this.sprinklerSplashRadius + 2));
-            let splatterImageIndex = Math.round(Math.getRandomArbitrary(96, 159));
-            if (splatterX - splatterRadius > 0 && splatterX + splatterRadius < 2047 && splatterY - splatterRadius > 0 && splatterY + splatterRadius < 2047)
-                events.push({
-                    unitId: this.id,
-                    ownerId: this.ownerId,
-                    type: 'sprinkle',
-                    x: splatterX,
-                    y: splatterY,
-                    radius: splatterRadius,
-                    inputIndex: splatterImageIndex
-                });
+            let distance = Math.getRandomArbitrary(this.sprinklerRangeMin, this.sprinklerRangeMax);
+            let direction = this.sprinklerAngle;
+            let radius = Math.round(Math.getRandomArbitrary(this.sprinklerSplashRadius - 2, this.sprinklerSplashRadius + 2));
+            for(let theta = 0; theta < Math.TWOPI; theta += Math.TWOPI / 5){
+                let event = this.createSplatter(distance, direction + theta, radius)
+                if(event)
+                    events.push(event);
+            }
+            
+
             this.lastSprinkleTimestamp = time;
         }
         events.push(...super.generateEvents(time));
 
         return events;
+    }
+
+    createSplatter(distance, direction, radius) {
+        let splatterX = Math.round(this.x + (distance * Math.cos(direction)));
+        let splatterY = Math.round(this.y + (distance * Math.sin(direction)));
+        let splatterImageIndex = Math.round(Math.getRandomArbitrary(0, 47));
+        if (splatterX - radius > 0 && splatterX + radius < 2047 && splatterY - radius > 0 && splatterY + radius < 2047)
+            return {
+                unitId: this.id,
+                ownerId: this.ownerId,
+                type: 'sprinkle',
+                x: splatterX,
+                y: splatterY,
+                radius: radius,
+                inputIndex: splatterImageIndex
+            };
     }
 
     updateProperties(time) {
